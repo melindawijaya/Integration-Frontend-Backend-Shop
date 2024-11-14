3b4495ae6009b164df32fd61e9f71161a4a75158
Melinda Wijaya <gmy4784@gmail.com>
2024-11-15 01:21:18 +0700

    feat: add token expiration check function using jwtDecode

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,46 @@
-import { useState, useEffect } from 'react'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
-import './App.css'
-import AboutView from './page/AboutView'
-import Navbar from './navbar/Navbar'
-import HomeView from './page/HomeView'
-import NotFoundView from './page/NotFoundView'
-import Login from './page/Login'
-import { RegisterPage } from './page/Register'
+import "./App.css";
+import Navbar from "./navbar/Navbar";
+import HomeView from "./page/HomeView";
+import Login from "./page/Login";
+import NotFoundView from "./page/NotFoundView";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  
-  // Cek apakah user sudah login atau belum
+  const location = useLocation();
+
   useEffect(() => {
     const token = localStorage.getItem("token");
     setIsAuthenticated(!!token);
   }, []);
 
-  const handleLogout = () => {
-    localStorage.removeItem("token")
-    localStorage.removeItem("username")
-    setIsAuthenticated(false)
-  }
-
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: isAuthenticated ? <HomeView /> : <Navigate to="/login" />
-    },
-    {
-      path: '/about',
-      element: <AboutView />
-    },
-    {
-      path: '/not-found',
-      element: <NotFoundView />
-    },
-    {
-      path: '/login',
-      element: isAuthenticated ? <Navigate to="/" /> : <Login/>
-    },
-    {
-      path: '*',
-      element: <NotFoundView/>
-    },
-    {
-      path: '/register',
-      element: <RegisterPage />
-    },
-  ]);
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    setIsAuthenticated(false);
+  };
 
   return (
     <>
-      {isAuthenticated && <Navbar onLogout={handleLogout}/>}
-      <RouterProvider router={router} />
+      {isAuthenticated && location.pathname !== "/login" && (
+        <Navbar onLogout={handleLogOut} />
+      )}
+
+      <Routes>
+        <Route
+          path="/"
+          element={isAuthenticated ? <HomeView /> : <Navigate to="/login" />}
+        />
+
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/" /> : <Login />}
+        />
+
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
     </>
   );
 }
